300551b51d812560f67ba2ecad2dfde5376e32c3
ckusellagussin <ckgussindev@googlemail.com>
2025-09-30 19:12:40 +0100

    Rebuild source code and updated to 1.6

---
+++ 1.6/Source/Learning_Overhaul/LearningOverhaulMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HarmonyLib;
+using Verse;
+
+namespace Learning_Overhaul
+{
+    public class LearningOverhaulMain : Mod
+    {
+        
+        public LearningOverhaulMain(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("CKG.LearningOverhaul");
+            harmony.PatchAll();
+        }
+        
+    }
+}
---
+++ 1.6/Source/Learning_Overhaul/LearningUtilityPatches_LearningRateFactor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Learning_Overhaul
+{
+    [HarmonyPatch(typeof(LearningUtility))]
+    [HarmonyPatch("LearningRateFactor")]
+    public static class LearningUtilityPatches_LearningRateFactor
+    {
+
+        private static void Postfix(Pawn pawn, ref float __result)
+        {
+            if (pawn.needs.joy != null)
+            {
+                if (pawn.needs.joy.CurCategory == JoyCategory.Low)
+                {
+                    __result *= 0.75f;
+                }
+
+                if (pawn.needs.joy.CurCategory == JoyCategory.VeryLow)
+                {
+                    __result *= 0.25f;
+                }
+
+                if (pawn.needs.joy.CurCategory == JoyCategory.Empty)
+                {
+                    __result *= 0.1f;
+                }
+            }
+        }
+    }
+}
