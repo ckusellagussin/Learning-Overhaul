5e8b99c2ef31a89d7eb60abfc13fe9e6823d004b
ckusellagussin <ckgussindev@googlemail.com>
2025-10-06 11:54:28 +0100

    Children have debuffs when their play need is low
    Moved files to the root folder to reorder folder structure

---
+++ Source/Learning_Overhaul/JoyUtilityPatches.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Learning_Overhaul
+{
+    public static class JoyUtilityPatches
+    {
+        // Token: 0x02000005 RID: 5
+        [HarmonyPatch(typeof(JoyUtility))]
+        [HarmonyPatch("JoyTickCheckEnd")]
+        private static class JoyUtilityPatches_JoyTickCheckEnd
+        {
+            // Token: 0x06000003 RID: 3 RVA: 0x0000206C File Offset: 0x0000026C
+            private static void Prefix(Pawn pawn, JoyTickFullJoyAction fullJoyAction = JoyTickFullJoyAction.EndJob, float extraJoyGainFactor = 1f, Building joySource = null)
+            {
+                if (ModsConfig.BiotechActive && pawn.needs.learning != null)
+                {
+                    float num = 0.5f;
+                    pawn.needs.learning.Learn(1.2E-05f * num * LearningUtility.LearningRateFactor(pawn));
+                }
+            }
+        }
+    }
+}
---
+++ Source/Learning_Overhaul/LearningOverhaulMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using RimWorld;
+using HarmonyLib;
+using Verse;
+
+namespace Learning_Overhaul
+{
+    public class LearningOverhaulMain : Mod
+    {
+        public LearningOverhaulMain(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("CKG.LearningOverhaul");
+            harmony.PatchAll();
+            
+            Log.Message("Learning Overhaul: Harmony patches applied");
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class LearningOverhaulStartup
+    {
+        static LearningOverhaulStartup()
+        {
+            ThoughtDef thoughtDef = DefDatabase<ThoughtDef>.GetNamedSilentFail("LowPlay");
+            if (thoughtDef != null)
+            {
+                Log.Message("Learning Overhaul: SUCCESS - LowPlay thought def found!");
+            }
+            else
+            {
+                Log.Error("Learning Overhaul: FAILED - LowPlay thought def NOT found!");
+                
+                // List ALL loaded thought defs to see what's available
+                Log.Message("=== ALL LOADED THOUGHT DEFS ===");
+                foreach (var def in DefDatabase<ThoughtDef>.AllDefs)
+                {
+                    Log.Message($"Thought def: {def.defName}");
+                }
+                Log.Message("=== END THOUGHT DEFS ===");
+            }
+        }
+    }
+}
